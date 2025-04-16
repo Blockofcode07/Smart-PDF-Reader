9bc02521b74e92f8a3220ae32af38d5df43fddb3
Aditya <adityajawanjal27@gmail.com>
2025-04-17 00:29:06 +0530

    Imported required libraries

---
+++ app.py
@@ -0,0 +1,13 @@
+
+# Task 1: Import the Libraries
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import PyPDFLoader
+from PyPDF2 import PdfReader, PdfWriter
+from tempfile import NamedTemporaryFile
+import base64
+from htmlTemplates import expander_css, css, bot_template, user_template
