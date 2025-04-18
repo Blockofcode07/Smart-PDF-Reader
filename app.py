499c1a9f09d3126fccd8608cfae0c9b82e0abba6
Aditya <adityajawanjal27@gmail.com>
2025-04-18 11:58:01 +0530

    Added functionality to display referenced PDF pages around user query context

--- app.py
+++ app.py
@@ -79,3 +79,33 @@ def main():
                     pdf = loader.load()
                     st.session_state.conversation = process_file(pdf)
                     st.session_state.col1.markdown("Done processing. You may now ask a question.")
+
+    # Task 7: Handle Query and Display Pages
+    if user_question:
+        handle_userinput(user_question)
+        with NamedTemporaryFile(suffix="pdf") as temp:
+            temp.write(st.session_state.pdf_doc.getvalue())
+            temp.seek(0)
+            reader = PdfReader(temp.name)
+            
+            pdf_writer = PdfWriter()
+            start = max(st.session_state.N-2, 0)
+            end = min(st.session_state.N+2, len(reader.pages)-1) 
+            while start <= end:
+                pdf_writer.add_page(reader.pages[start])
+                start+=1
+            with NamedTemporaryFile(suffix="pdf") as temp2:
+                pdf_writer.write(temp2.name)
+                with open(temp2.name, "rb") as f:
+                    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+                    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}#page={3}"\
+                        width="100%" height="900" type="application/pdf frameborder="0"></iframe>'
+                
+                    st.session_state.col2.markdown(pdf_display, unsafe_allow_html=True)   
+
+
+
+
+if __name__ == '__main__':
+    main()
