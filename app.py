4809dec91ca0e2e41b8c6d4cfe1c0fa16433aa78
Aditya <adityajawanjal27@gmail.com>
2025-04-18 11:42:59 +0530

    Create the Web Page Layout with the help of Streamlit lib

--- app.py
+++ app.py
@@ -11,3 +11,30 @@ from PyPDF2 import PdfReader, PdfWriter
 from tempfile import NamedTemporaryFile
 import base64
 from htmlTemplates import expander_css, css, bot_template, user_template
+
+
+
+
+
+def main():
+    pass
+    
+    # Task 3: Create Web-page Layout
+    load_dotenv()
+    st.set_page_config(layout="wide", 
+                       page_title="Interactive Reader",
+                       page_icon=":books:")
+    
+    st.write(css, unsafe_allow_html=True)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+    if "N" not in st.session_state:
+        st.session_state.N = 0
+   
+    st.session_state.col1, st.session_state.col2 = st.columns([1, 1])
+    st.session_state.col1.header("Interactive Reader :books:")
+    user_question = st.session_state.col1.text_input("Ask a question on the contents of the uploaded PDF:")
+    st.session_state.expander1 = st.session_state.col1.expander('Your Chat', expanded=True)
+    st.session_state.col1.markdown(expander_css, unsafe_allow_html=True) 
