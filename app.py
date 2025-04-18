e6ee099646d099fce4ed32a9ab1252955d4bc141
Aditya <adityajawanjal27@gmail.com>
2025-04-18 11:54:53 +0530

    Implemented chatbot response handling and chat history updates

--- app.py
+++ app.py
@@ -27,6 +27,20 @@ def process_file(doc):
         return_source_documents=True)
     return chain
 
+# Task 6: Method for Handling User Input
+def handle_userinput(query):
+
+    response = st.session_state.conversation({"question": query, 'chat_history':st.session_state.chat_history}, return_only_outputs=True)
+    st.session_state.chat_history += [(query, response['answer'])]
+
+    st.session_state.N = list(response['source_documents'][0])[1][1]['page']
+    
+    
+    for i, message in enumerate(st.session_state.chat_history): 
+         st.session_state.expander1.write(user_template.replace("{{MSG}}", message[0]), unsafe_allow_html=True)
+         st.session_state.expander1.write(bot_template.replace("{{MSG}}", message[1]), unsafe_allow_html=True)
+
+
 
 def main():
     pass
