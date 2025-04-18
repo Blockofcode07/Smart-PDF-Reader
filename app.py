a726b8f82228b9fad10cad36e6e60c7741654c93
Aditya <adityajawanjal27@gmail.com>
2025-04-18 11:50:20 +0530

    Added file upload and processing flow with spinner and user notifications

--- app.py
+++ app.py
@@ -12,8 +12,20 @@ from tempfile import NamedTemporaryFile
 import base64
 from htmlTemplates import expander_css, css, bot_template, user_template
 
+# Task 4: Process the Input PDF
+def process_file(doc):
 
+    model_name = "thenlper/gte-small"
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+    embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs)  
+    
+    pdfsearch = Chroma.from_documents(doc, embeddings)
 
+    chain = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.3), 
+        retriever=pdfsearch.as_retriever(search_kwargs={"k": 2}),
+        return_source_documents=True)
+    return chain
 
 
 def main():
@@ -38,3 +50,18 @@ def main():
     user_question = st.session_state.col1.text_input("Ask a question on the contents of the uploaded PDF:")
     st.session_state.expander1 = st.session_state.col1.expander('Your Chat', expanded=True)
     st.session_state.col1.markdown(expander_css, unsafe_allow_html=True) 
+
+    # Task 5: Load and Process the PDF 
+    st.session_state.col1.subheader("Your documents")
+    st.session_state.pdf_doc = st.session_state.col1.file_uploader("Upload your PDF here and click on 'Process'")
+
+    if st.session_state.col1.button("Process", key='a'):
+        with st.spinner("Processing"):
+            if st.session_state.pdf_doc is not None:
+                with NamedTemporaryFile(suffix="pdf") as temp:
+                    temp.write(st.session_state.pdf_doc.getvalue())
+                    temp.seek(0)
+                    loader = PyPDFLoader(temp.name)
+                    pdf = loader.load()
+                    st.session_state.conversation = process_file(pdf)
+                    st.session_state.col1.markdown("Done processing. You may now ask a question.")
